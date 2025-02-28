0a52993bf483012c03e4e6dd7ed2fd5124f15def
q <phamousq@gmail.com>
2025-02-28 16:36:51 -0600

    vercel deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,9 +12,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			edge: false,
-			split: true
-		  })
+			runtime: 'nodejs18.x',
+		  }
 	}
 };
 
