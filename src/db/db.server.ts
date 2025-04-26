cb6dd0fb2e935d4c0ac9a95c2ec02e92844b90ea
q <phamousq@gmail.com>
2025-04-26 14:19:39 -0500

    remove database..

--- src/db/db.server.ts
+++ src/db/db.server.ts
@@ -1,11 +1,11 @@
-import 'dotenv/config';
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from './schema';
+// import 'dotenv/config';
+// import { neon } from '@neondatabase/serverless';
+// import { drizzle } from 'drizzle-orm/neon-http';
+// import * as schema from './schema';
 
-const connectionString: string = process.env.VITE_DATABASE_URL as string;
+// const connectionString: string = process.env.VITE_DATABASE_URL as string;
 
-const sql = neon(connectionString);
-const db = drizzle(sql, { schema });
+// const sql = neon(connectionString);
+// const db = drizzle(sql, { schema });
 
-export default db;
+// export default db;
